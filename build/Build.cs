906f66736aa67278aaba44e929b16c0df0547ccb
devops <devops@jetlinx.com>
2024-10-30 13:55:07 -0500

    wip

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
@@ -9,20 +10,24 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.NerdbankGitVersioning;
+using Nuke.Common.Tools.NuGet;
 using Nuke.Common.Utilities.Collections;
+using Serilog;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.NuGet.NuGetTasks;
+using static Nuke.Common.IO.HttpTasks;
 
 [GitHubActions(
     "ubuntu-latest",
     GitHubActionsImage.UbuntuLatest,
     FetchDepth = 0,
     //OnPushBranchesIgnore = new[] {"main"},
-    OnPushBranches = new[] { "main" },
+    OnPushBranches = new[] {"main"},
     InvokedTargets = new[] {nameof(Push)},
     EnableGitHubToken = true,
     PublishArtifacts = false,
-    ImportSecrets = new[] { "NUGET_PUBLISH_KEY" }
-    )]
+    ImportSecrets = new[] {"NUGET_PUBLISH_KEY"}
+)]
 [DotNetVerbosityMapping]
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
@@ -61,7 +66,7 @@ class Build : NukeBuild
             var frameworks = from project in Solution.Projects
                 from framework in project.GetTargetFrameworks()
                 where project.Name == "Esc.Sdk.Cli"
-                select (project,framework);
+                select (project, framework);
 
             DotNetPublish(_ => _
                 .SetConfiguration(Configuration)
@@ -80,23 +85,67 @@ class Build : NukeBuild
             );
         });
 
+    Target Download => _ => _
+        .Executes(() =>
+        {
+            const string version = "v0.11.0";
+            const string baseUrl = $"https://get.pulumi.com/esc/releases/esc-{version}";
+
+            List<string> operatingSystems =
+            [
+                "windows",
+                "linux",
+                "darwin"
+            ];
+
+            foreach (var operatingSystem in operatingSystems)
+            {
+                var extension = operatingSystem == "windows" ? "zip" : "tar.gz";
+
+                var url = $"{baseUrl}-{operatingSystem}-x64.{extension}";
+                var filename = $"esc-{version}-{operatingSystem}-x64.{extension}";
+
+                Log.Information("url: {0} filename: {1}", url, filename);
+
+                AbsolutePath releaseTempFilename = Path.Combine(TemporaryDirectory, filename);
+                HttpDownloadFile(url, releaseTempFilename);
+
+                var unCompressDirectory = TemporaryDirectory / operatingSystem;
+                releaseTempFilename.UncompressTo(unCompressDirectory);
+
+                unCompressDirectory.GlobFiles("**/esc*").ForEach(file =>
+                {
+                    var contentFilesDirectoryName = operatingSystem == "windows"
+                        ? "win64"
+                        : $"{operatingSystem}64";
+
+                    file.Move(RootDirectory / "contentFiles" / contentFilesDirectoryName / file.Name);
+                });
+            }
+        });
+
     [Parameter]
     string NuGetSource => "https://api.nuget.org/v3/index.json";
 
     AbsolutePath OutputDirectory => RootDirectory / "output";
 
     Target Pack => _ => _
-        .DependsOn(Compile)
+        .DependsOn(Download, Compile)
         .Produces(PackagesDirectory / "*.nupkg")
         .Executes(() =>
         {
+            NuGetPack(_ => _
+                .SetTargetPath("Esc.nuspec")
+                .SetOutputDirectory(PackagesDirectory)
+                .SetVersion(NerdbankGitVersioning.NuGetPackageVersion)
+            );
+
             DotNetPack(_ => _
                 .SetConfiguration(Configuration)
                 .SetNoBuild(SucceededTargets.Contains(Compile))
                 .SetOutputDirectory(PackagesDirectory)
                 .SetRepositoryUrl(GitRepository.HttpsUrl)
                 .SetVersion(NerdbankGitVersioning.NuGetPackageVersion)
-                
             );
 
             ReportSummary(_ => _
